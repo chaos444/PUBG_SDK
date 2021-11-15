ce5b50b8f9b239d70e60b4534191e808d5625965
chaos444 <981949@qq.com>
2021-11-15 08:56:38 +0800

    14.2.5.11
    14.2.5.11

---
+++ PUBG_Offset.hpp
@@ -0,0 +1,52 @@
+// AActor	Type:class USceneComponent*
+RootComponent	=	0x260,//(0x8)
+// USceneComponent	Type:struct FRotator
+RelativeRotation	=	0x218,//(0xC)
+// USceneComponent	Type:struct FVector
+ComponentVelocity	=	0x360,//(0xC)
+// USkeletalMeshComponent	Type:class UAnimInstance*
+AnimScriptInstance	=	0xCA0,//(0x8)
+// UCharacterMovementComponent	Type:float
+MaxAcceleration	=	0x318,//(0x4)
+// UCharacterMovementComponent	Type:struct FVector
+Acceleration	=	0x3B8,//(0xC)
+// UCharacterMovementComponent	Type:struct FVector
+LastUpdateVelocity	=	0x3E0,//(0xC)
+// ACharacter	Type:class USkeletalMeshComponent*
+Mesh	=	0x570,//(0x8)
+// AController	Type:class APawn*
+Pawn	=	0x470,//(0x8)
+// APlayerController	Type:class APlayerCameraManager*
+PlayerCameraManager	=	0x4C0,//(0x8)
+// APlayerCameraManager	Type:struct FCameraCacheEntry
+CameraCache	=	0x460,//(0x5C0)
+// Class:FMinimalViewInfo	Type:struct FRotator
+Rotation	=	0x18,//(0xC)
+// Class:FMinimalViewInfo	Type:float
+FOV	=	0x2C,//(0x4)
+// Class:FMinimalViewInfo	Type:struct FVector
+Location	=	0x5A4,//(0xC)
+// ATslCharacter	Type:class UWeaponProcessorComponent*
+WeaponProcessor	=	0xDE0,//(0x8)
+// ATslCharacter	Type:int
+LastTeamNum	=	0xEC0,//(0x4)
+// ATslCharacter	Type:struct FString
+CharacterName	=	0x1108,//(0x10)
+// ATslCharacter	Type:int
+SpectatedCount	=	0x1168,//(0x4)
+// ATslCharacter	Type:float
+Health	=	0x11F8,//(0x4)
+// ATslCharacter	Type:float
+GroggyHealth	=	0x19D4,//(0x4)
+// UWeaponTrajectoryData	Type:struct FWeaponRecoilConfig
+RecoilConfig	=	0x48,//(0xD0)
+// UWeaponTrajectoryData	Type:struct FWeaponTrajectoryConfig
+TrajectoryConfig	=	0x118,//(0x48)
+// ATslWeapon_Trajectory	Type:class UWeaponTrajectoryData*
+WeaponTrajectoryData	=	0xF08,//(0x8)
+// UWeaponProcessorComponent	Type:TArray<class ATslWeapon*>
+EquippedWeapons	=	0x2C8,//(0x10)
+// UWeaponProcessorComponent	Type:struct FWeaponArmInfo
+WeaponArmInfo	=	0x2E8,//(0x5)
+// UCrowdFollowingComponent	Type:class UCharacterMovementComponent*
+CharacterMovement	=	0x4F0,//(0x8)
